0c9549077cd1cf6434e12d1890f72bf0343f5b31
Donald Kagunila <donkagunila@gmail.com>
2019-04-03 05:53:34 +0300

    project structure -cont

---
+++ resources/views/app/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.master')
+
+@section('title', 'Mishe API')
+
+@section('contents')
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="landing">
+					<div class="page-title">
+						<h2>
+							Mishe API
+						</h2>
+
+						<div class="line-sep"></div>
+
+					</div>
+
+					<div class="action">
+						<a class="btn btn-secondary">
+							Docs
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+@endsection
